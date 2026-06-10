488e710421dccc95a30accfd780f4506c324d970
daarius10 <datcuioandatcu11@gmail.com>
2026-06-10 21:08:48 +0200

    Create data-equipos.js

---
+++ base_datos/equipos/data-equipos.js
@@ -0,0 +1,11 @@
+// Simulación de la tabla de Equipos y sus Plantillas
+const DB_TEAMS = [
+  { id: 1, name: "Atlético FC", group: "A", players: [{ num: 1, name: "Carlos Ruiz", pos: "Portero" }, { num: 10, name: "Marcos López", pos: "Delantero" }] },
+  { id: 2, name: "Real Sporting", group: "A", players: [{ num: 1, name: "Juan García", pos: "Portero" }, { num: 9, name: "Adrián Moreno", pos: "Delantero" }] },
+  { id: 3, name: "Deportivo Sur", group: "A", players: [{ num: 1, name: "Pablo Jiménez", pos: "Portero" }] },
+  { id: 4, name: "FC Estrella", group: "A", players: [{ num: 1, name: "Manuel Díaz", pos: "Portero" }] },
+  { id: 5, name: "Unión Norte", group: "B", players: [{ num: 1, name: "Alejandro Gil", pos: "Portero" }] },
+  { id: 6, name: "CD Rápido", group: "B", players: [{ num: 1, name: "Nicolás Pérez", pos: "Portero" }] },
+  { id: 7, name: "Atlético Oeste", group: "B", players: [{ num: 1, name: "David Molina", pos: "Portero" }] },
+  { id: 8, name: "Sporting Club", group: "B", players: [{ num: 1, name: "Roberto Suárez", pos: "Portero" }] }
+];
