8c58866990ad02deb6520760f51b7783f3d564ec
daarius10 <datcuioandatcu11@gmail.com>
2026-06-10 21:07:29 +0200

    Create data-config.js

---
+++ base_datos/config/data-config.js
@@ -0,0 +1,6 @@
+// Simulación de la tabla de Configuración General
+const DB_CONFIG = {
+  name: "Santiago Cup",
+  season: "Temporada 2025-26",
+  desc: "Sigue en tiempo real todos los partidos, grupos y la clasificación del torneo."
+};
